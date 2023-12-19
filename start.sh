3178529cb2c59e8c69ad84579fe3c74c7c475435
alezi-n <study.alez@gmail.com>
2023-12-19 00:05:20 -0300

    update

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Este comando inicia o servidor Uvicorn apontando para o seu arquivo principal (main.py)
-# Ele configura o host para 0.0.0.0 para que o aplicativo seja acessível externamente
-# e usa a variável de ambiente $PORT fornecida pelo Railway para definir a porta.
-exec uvicorn main:app --host 0.0.0.0 --port $PORT --reload
